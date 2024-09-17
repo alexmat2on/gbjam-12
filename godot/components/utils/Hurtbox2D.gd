1f7b35731bbf062adcff5fd323240c9f48481549
Matthew Laikhram <mlaikhram@gmail.com>
2024-09-17 01:52:23 -0400

    added utility scripts, behavior scripts, and cleaned up some initial config

---
+++ godot/components/utils/Hurtbox2D.gd
@@ -0,0 +1,21 @@
+class_name Hurtbox2D
+extends Area2D
+
+
+@export var can_take_damage = true
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if can_take_damage:
+		connect("area_entered", self._on_hitbox_entered)
+
+
+func _on_hitbox_entered(hitbox):
+	if hitbox == null || !hitbox is Hitbox2D:
+		return
+	
+	if owner.has_method("take_damage"):
+		owner.take_damage(hitbox)
+	else:
+		print("owner does not have take_damage")
