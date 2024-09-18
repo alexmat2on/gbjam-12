03be74e30595f045edb036105e654e7e58e3026d
Alex <alexmat2on@protonmail.com>
2024-09-17 23:47:39 -0400

    Add Interactible2D

--- godot/components/player/player.gd
+++ godot/components/player/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 class_name Player
 
-@onready var animSprite = $AnimatedSprite2D
 @onready var _animated_sprite = $AnimatedSprite2D
-@onready var _interactor = $Interactor2D
+@onready var _interactor = $Interactor
+
+var current_interactible = null
 
 const playerSpeed := 250
 const jumpSpeed := 200
@@ -11,13 +12,14 @@ const gravity := 15
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_interactor.connect("area_entered", self._on_interaction_area_entered)
+	_interactor.connect("area_exited", self._on_interaction_area_exited)
 	_animated_sprite.play("idle")
 
 func _physics_process(_delta):
-	if _interactor.has_interaction() && Input.is_action_just_pressed("start"):
-		PlayerVariables.remove_health()
-		print("interactive start pressed!")
-	
+	if is_instance_valid(current_interactible) && Input.is_action_just_pressed("start"):
+		current_interactible.interact()
+		
 	var horizontal_direction := Input.get_axis("left", "right")
 		
 	if Input.is_action_pressed("up") and is_on_floor():
@@ -36,3 +38,15 @@ func _physics_process(_delta):
 	velocity.x = horizontal_direction * playerSpeed
 	
 	move_and_slide()
+
+func _on_interaction_area_entered(interactible: Area2D):
+	print("interaction entered!")
+	if interactible.get_parent() is Interactible2D:
+		current_interactible = interactible.get_parent()
+		current_interactible.on_interact_enter()
+
+func _on_interaction_area_exited(interactible: Area2D):
+	print("interaction exit!")
+	if interactible.get_parent() == current_interactible:
+		current_interactible.on_interact_exit()
+		current_interactible = null
---
+++ godot/components/utils/Interactible2D.gd
@@ -0,0 +1,15 @@
+class_name Interactible2D
+extends Node2D
+
+@onready var popup = get_node_or_null("Popup")
+
+func interact():
+	pass 
+
+func on_interact_enter():
+	if is_instance_valid(popup):
+		popup.show()
+
+func on_interact_exit():
+	if is_instance_valid(popup):
+		popup.hide()
