1cace95921a3bb8e8c7e81ae31371c0d81e9b714
Matthew Laikhram <mlaikhram@gmail.com>
2024-09-23 00:28:06 -0400

    round timers and phase change

--- godot/scripts/utils/GlobalState.gd
+++ godot/scripts/utils/GlobalState.gd
@@ -1,25 +1,82 @@
 extends Node
+class_name Global
 
 enum GameState {
+	SETUP,
 	GATHER,
 	SERVE
 }
 
+const gather_levels = [
+	"graveyard"
+]
+
+const ROUND_TIME = 120.0
+
 var _day: int = 0
 var _orders: int = 0
-var _game_state: GameState = GameState.GATHER
+var _game_state: GameState = GameState.SETUP
 
 var player_tool_a: Enums.Tool = Enums.Tool.NONE
 var player_tool_b: Enums.Tool = Enums.Tool.NONE
 
+var _time_left: float = ROUND_TIME
+
+var enter_serve_mode_menu: OptionMenu = load("res://resources/enter_serve_mode_menu.tres")
+
 
 func clear():
-	_game_state = GameState.GATHER
+	get_tree().node_added.connect(on_node_added)
+	_game_state = GameState.SETUP
 	Inventory.clear()
 	_day = 0
 	_orders = 0
 	player_tool_a = Enums.Tool.NONE
 	player_tool_b = Enums.Tool.NONE
+	
+	_time_left = ROUND_TIME
+	SignalBus.time_updated.emit(_time_left)
+	
+func on_node_added(node: Node):
+	if _game_state != GameState.SETUP:
+		print("not setup")
+		return
+	elif gather_levels.any(func(n): return n == node.name):
+		get_tree().node_added.disconnect(on_node_added)
+		_game_state = GameState.GATHER
+		
+func _physics_process(delta):
+	match _game_state:
+		GameState.GATHER:
+			_gather_physics_process(delta)
+		GameState.SERVE:
+			_serve_physics_process(delta)
+
+func _gather_physics_process(delta):
+	_tick_time(delta)
+	if _time_left <= 0:
+		SignalBus.selected_option.connect(on_option_selected)
+		SignalBus.opened_option_menu.emit(enter_serve_mode_menu, false)
+		# TODO: switch global state to a scene and use the option menu handler
+
+func _serve_physics_process(delta):
+	_tick_time(delta)
+	if _time_left <= 0:
+		print("time up!")
+
+func on_option_selected(id):
+	SignalBus.selected_option.disconnect(on_option_selected)
+	match _game_state:
+		GameState.GATHER:
+			# TODO: switch global state to a scene and use the option menu handler
+			get_tree().change_scene_to_file("res://scenes/levels/campsite.tscn")
+			_game_state = GameState.SERVE
+			_time_left = ROUND_TIME
+			
+func _tick_time(delta):
+	_time_left = max(0.0, _time_left - delta)
+	SignalBus.time_updated.emit(_time_left)
+	
 
 func get_day() -> int:
 	return _day
